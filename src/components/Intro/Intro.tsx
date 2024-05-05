4b4fcaed5b5fad60a06e0fb5df83905c7e20a764
Adrián Abal Domínguez <aabaldominguez@gmail.com>
2024-05-05 13:52:20 +0200

    Add Intro component with membership form

---
+++ src/components/Intro/Intro.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import './Intro.scss'
+import Subscribe from './Susbcribe/Subscribe';
+
+const Intro = () => {
+
+  const [open, setOpen] = useState(false);
+
+  const handleOpenDialog = () => {
+    setOpen(!open);
+  }
+
+  const handleCloseDialog = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <div className="Intro">
+        <div className="Wrapper">
+          <div className={open ? `Intro-center isHide` : `Intro-center`}>
+            <h2 className="Intro-h2">Películas y series sin límites y mucho más</h2>
+            <h3 className="Intro-h3">Disfruta donde quieras. Cancela cuando quieras.</h3>
+            <button className='Intro-start' onClick={handleOpenDialog} type='button' aria-label='Empezar' >Empezar</button>
+          </div>
+        </div>
+      </div>
+      <Subscribe isOpen={open} onClose={handleCloseDialog}/>
+    </>
+  )
+}
+
+export default Intro;
---
+++ src/components/Intro/Susbcribe/Subscribe.tsx
@@ -0,0 +1,44 @@
+import './Subscribe.scss'
+
+/* interface SubscribeProps {
+  isOpen: boolean;
+  onClose: () => void
+} */
+
+// const Subscribe: React.FC<SubscribeProps> = ({ isOpen, onClose }) => {
+const Subscribe = ({ isOpen, onClose }:{ isOpen: boolean, onClose: () => void } ) => {
+  return (
+    <>
+      { isOpen && ( 
+        <dialog className="Subscribe" open={isOpen}>
+          <div className="Subscribe-content">
+            <header className="Subscribe-header">
+              <button className="Subscribe-button"  type="button" aria-label="Cerrar">
+                  <svg className="Subscribe-svg" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+                  </svg>
+              </button>
+              <h2 className="Subscribe-h2">Crear o reanudar suscripción</h2>
+            </header>
+            <div className="Subscribe-body">
+              <p className="Subscribe-p">¿Quieres ver algo ya? Escribe tu dirección de correo para crear una suscripción a Netflix o reactivarla.</p>
+              {/* Create or reactivate membership form */}           
+              <form className="Subscribe-form Form" action="#" method="post">
+                <div className="Form-group Form-group--email">
+                  <input id="focusEmail" className="Form-input" type="email" placeholder="" required/>
+                  <label className="Form-label" htmlFor="focusEmail">Dirección de correo</label>
+                </div>
+                <div className="Form-group Form-group--controller">
+                  <button className="Form-button Form-button--back"  onClick={onClose} type="button" arial-label="Volver">Volver</button>
+                  <button className="Form-button Form-button--continue" type="submit" arial-label="Continuar">Continuar</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </dialog>
+      )}
+    </>
+  )
+}
+
+export default Subscribe;
