4d8525f7bcbabdeb764cfd23c47cd5eb9e94bbea
Adrián Abal Domínguez <aabaldominguez@gmail.com>
2024-06-08 18:12:40 +0200

    Refactoring of the logo to a stand-alone component and several fixes

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,34 +1,37 @@
 import './Footer.scss'
 import { LINKS } from "../../consts/links-data";
 import Languages from '../Languages/Languages';
+import Logo from '../Logo/Logo';
 
 const Footer = () => {
     return (
-        <>
-          <footer className="Footer">
-            <div className="Footer-center">
-              <div className="Footer-column">
-                <p className="Footer-p">
-                  ¿Preguntas? Llama al <a className="Footer-phone" href="tel:900-759-106" title="Tel:">900-759-106</a>
-                </p>
-                <ul className="Footer-links">
-                  {
-                    LINKS.map(({id, name, url}) => {
-                      return (
-                        <li className="Footer-link" key={id}>
-                          <a className="Footer-a" href={url} title={name}>{name}</a>
-                        </li>
-                      )
-                    })
-                  }
-                </ul>
-                <Languages />
-                <span className="Footer-span">Netflix España</span>
+      <>
+        <footer className="Footer">
+          <div className="Footer-center">
+            <div className="Footer-column">
+              <p className="Footer-p">
+                ¿Preguntas? Llama al <a className="Footer-phone" href="tel:900-759-106" title="Tel:">900-759-106</a>
+              </p>
+              <ul className="Footer-links">
+                {
+                  LINKS.map(({id, name, url}) => {
+                    return (
+                      <li className="Footer-link" key={id}>
+                        <a className="Footer-a" href={url} title={name}>{name}</a>
+                      </li>
+                    )
+                  })
+                }
+              </ul>
+              <Languages />
+              <div className="Footer-logo">
+                <Logo />
               </div>
             </div>
-          </footer>
-        </>
+          </div>
+        </footer>
+      </>
     )
 }
 
-export default Footer;
+export default Footer
